2bf57bcda84082b66220b438376d0bbad42b2348
Karl Jose Buena <karljosebuena@Karls-MacBook-Pro.local>
2020-04-27 16:23:21 +0800

    Added and explored yargs.

--- notesapp/app.js
+++ notesapp/app.js
@@ -1,6 +1,60 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const notes = require('./notes')();
 
-notes.getNotes();
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(chalk.inverse('Success!'));
+// add
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        },
+    },
+    handler: (argv) => {
+        const { title, body } = argv;
+        console.log('Adding a new note!')
+        console.log(chalk.green(`Title: ${title}`));
+        console.log(chalk.blue(`Body: ${body}`));
+    }
+});
+
+// remove
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: () => {
+        console.log('Removing a new note!')
+    }
+});
+
+// read
+yargs.command({
+    command: 'read',
+    describe: 'Read a new note',
+    handler: () => {
+        console.log('Readinging a new note!')
+    }
+});
+
+
+// list
+yargs.command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: () => {
+        console.log('Linsting all notes!')
+    }
+});
+
+yargs.parse();
